601f87147f7291e95c91010ced2a4aaa27ac5a96
NussSchmatze <34706867+Aldaladdich@users.noreply.github.com>
2018-03-08 11:12:09 +0100

    Add files via upload

--- src/Copy4Synapse.cs
+++ src/Copy4Synapse.cs
@@ -1,19 +1,17 @@
 ﻿using System;
 using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Copy4Synapse {
     class Copy4Synapse {
         static void Main() {
+        	bool confirmed = false;
             string source;
             string target;
             string destFile;
             string fileName;
             string sbstr;
 		
-		// Streamreader instanziieren 
+		    // Streamreader instanziieren 
             StreamReader path = new StreamReader("../path/conf.ini");
             //erste Zeile der Datei wird übersprungen
             path.ReadLine();            
@@ -33,7 +31,6 @@ namespace Copy4Synapse {
             if (System.IO.Directory.Exists(source)){
                 Console.WriteLine("Quellpfad existiert!");
                 Console.WriteLine("");
-
             	//alle dateien im Vz werden in diesem StringArray hinterlegt
                 string[] files = System.IO.Directory.GetFiles(source);
 
@@ -49,20 +46,37 @@ namespace Copy4Synapse {
                 }else{
                     Console.WriteLine("Zielpfad existiert nicht");
                     Console.WriteLine("");
-                    Console.WriteLine("Soll das Verzeichnis erstellt werden? Drücke <Enter> zum erstellen/ <ESC> zum abbrechen");
-                    cki = Console.ReadKey();
-					if(ConsoleKey.Enter != 0){
-                        DirectoryInfo dir = Directory.CreateDirectory(target);
-                        Console.WriteLine("Das Verzeichnis wurde erstellt am {0}.", Directory.GetCreationTime(target));
-						foreach (string s in files) {
-							fileName = System.IO.Path.GetFileName (s);
-							destFile = System.IO.Path.Combine (target, fileName);
-							System.IO.File.Copy (s, destFile, true);
-						}
-                    }
+                    do {
+
+					ConsoleKey response;
+					    do
+					    {
+					        Console.Write("Soll das Verzeichnis erstellt werden ?(j/n)");
+					        response = Console.ReadKey(false).Key;   // true is intercept key (dont show), false is show
+					        if (response != ConsoleKey.Enter)
+					            Console.WriteLine();
+
+					    } while (response != ConsoleKey.J && response != ConsoleKey.N);
+
+                            if (response == ConsoleKey.J){
+                                DirectoryInfo dir = Directory.CreateDirectory(target);
+                                Console.WriteLine("Das Verzeichnis wurde erstellt am {0}.", Directory.GetCreationTime(target));
+                                foreach (string s in files) {
+                                    fileName = System.IO.Path.GetFileName (s);
+                                    destFile = System.IO.Path.Combine (target, fileName);
+                                    System.IO.File.Copy (s, destFile, true);
+                                }
+                                confirmed = response == ConsoleKey.J;
+                            }else if (response == ConsoleKey.N){
+                                Console.WriteLine("Das Verzeichnis wurde nicht erstellt!");
+                                confirmed = response == ConsoleKey.N;
+                            }
+
+					} while (!confirmed);
+					Console.ReadLine();
+                    //Console.ReadKey().Key	
                 }
             }else{
-
                 //falls Quellpfad nicht vorhanden gib einen Fehler aus
                 Console.WriteLine("Quellpfad existiert nicht");
             }
